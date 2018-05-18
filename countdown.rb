8721540a0d6502437d3739a62de3e41e9cd3e247
William Li <githuberr@outlook.com>
2018-05-18 22:24:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
 def countdown(num)
-  while num>0
+  while num>=0
     puts "#{num} SECOND(S)!"
     num -= 1
   end
@@ -9,7 +9,7 @@ def countdown(num)
 end
 
 def countdown(num)
-  while num>0
+  while num>=0
     puts "#{num} SECOND(S)!"
     num -= 1
     sleep(1)
